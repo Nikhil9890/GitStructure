d81a928283eca3c98291fd3e1ee177bfc5e6fdc6
nsdeshmukhnd <nsdeshmukh78@gmail.com>
2022-12-22 15:01:06 +0530

    This is Karate Framework

---
+++ API_Karate.feature
@@ -0,0 +1,4 @@
+Feature:fetching User Details
+Scenario:testing the get call for User Details
+Given URL 'https://reqres.in/api/user/2'
+Then status 201
---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.junit4.Karate;
+
+@RunWith(Karate.class)
+public class TestRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
